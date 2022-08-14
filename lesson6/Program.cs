6fec267b21a0696f2263757faf6ad58e57a81b83
Konstantin <konstantin.kif@mail.ru>
2022-08-14 23:33:55 +0700

    add lesson6

---
+++ lesson6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "константин")
+{
+    Console.WriteLine("Ура, это же Константин!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
