9a73a9586baeedd231b6931a68fe70e7b8777fda
Konstantin <konstantin.kif@mail.ru>
2022-08-21 18:02:41 +0700

    add lesson15

---
+++ lesson15/Program.cs
@@ -0,0 +1,20 @@
+﻿string Method4(int count, string text)
+{
+    string result = String.Empty;
+    for (int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+string result = Method4(1, "таблица умнажения");
+Console.WriteLine(result);
+
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 2; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} * {j} = {i * j}");
+    }
+    Console.WriteLine();
+}
