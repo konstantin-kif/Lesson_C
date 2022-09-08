34f0e0d8624db91a6f5bbe95e247e2d958fcf1ba
Konstantin <konstantin.kif@mail.ru>
2022-09-08 13:42:24 +0700

    add lesson18

--- lesson18/Program.cs
+++ lesson18/Program.cs
@@ -2,11 +2,11 @@
 
 // строки с 1, а столбцы с 0 пример: 
 // table[1,0] table[1,1] table[1,2] table[1,3] ...
-string[,] table = new string[2, 5];
+string[,] table = new string[2, 4];
 table[1, 2] = "text";
 for (int rows = 0; rows < 2; rows++)
 {
-    for (int columns = 0; columns < 5; columns++)
+    for (int columns = 0; columns < 4; columns++)
     {
         Console.Write($"/{table[rows, columns]}/");
     }
@@ -23,3 +23,32 @@ for (int i = 0; i < matrix.GetLength(0); i++)
     }
     Console.WriteLine();
 }
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10);  // Генератотор случайных чисел
+        }
+    }
+}
+
+int[,] matr = new int[3, 4];
+PrintArray(matr);
+FillArray(matr);
+ Console.WriteLine();
+PrintArray(matr);
